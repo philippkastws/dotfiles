b1fff19fe2b929b38175472abd2d66ff9b7aaee3
Philipp Kast <philipp.kast@autodesk.com>
2025-09-09 12:40:33 +0200

    Use block for normal mode

--- fish/config.fish
+++ fish/config.fish
@@ -24,7 +24,7 @@ alias bat='bat --paging=always'
 set -gx EDITOR nvim
 
 set fish_cursor_insert block
-set fish_cursor_default line
+set fish_cursor_default block
 
 # Install node on cd
 #function __check_nvm --on-variable PWD --description 'Do nvm stuff'
