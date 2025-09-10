ebfc2969563010879d461a5e7df0f942e9d47008
Philipp Kast <philipp.kast@autodesk.com>
2025-09-10 16:08:52 +0200

    Add claude alias

--- fish/config.fish
+++ fish/config.fish
@@ -138,3 +138,6 @@ bind Y fish_clipboard_copy
 # in visual mode, copy and exit visual mode
 bind -s --preset -M visual -m default Y "fish_clipboard_copy; commandline -f end-selection repaint-mode"
 bind p fish_clipboard_paste
+
+alias claude='/Users/kast/.claude/local/claude'
+
