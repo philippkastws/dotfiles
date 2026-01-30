405cef845d30ada2c446702eff4d1cb96b47c3d7
Philipp Kast <philipp.kast@autodesk.com>
2026-01-30 16:53:25 +0100

    Add fish_user_key_bindings

---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,13 @@
+function fish_user_key_bindings
+  fzf_key_bindings
+  bind --erase \cr
+
+  # Yank selection to system clipboard in visual mode
+  bind -M visual y fish_clipboard_copy
+  
+  # Yank entire line to system clipboard in normal mode
+  bind -M normal yy fish_clipboard_copy
+  
+  # Optional: Paste from system clipboard with 'p'
+  bind -M normal p fish_clipboard_paste
+end
