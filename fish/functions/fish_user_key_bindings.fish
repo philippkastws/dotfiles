2c577460e118844c6515d0d1cbff94521e430ffb
philippkastws <philipp@kast.ws>
2026-03-05 20:49:22 +0100

    Fix vim yank and paste (Y, P)

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,11 +3,12 @@ function fish_user_key_bindings
   bind --erase \cr
 
   # Yank selection to system clipboard in visual mode
-  bind -M visual y fish_clipboard_copy
+  bind -M visual Y fish_clipboard_copy
   
   # Yank entire line to system clipboard in normal mode
-  bind -M normal yy fish_clipboard_copy
+  # bind -M normal yy fish_clipboard_copy
   
   # Optional: Paste from system clipboard with 'p'
-  bind -M normal p fish_clipboard_paste
+  # bind -M normal p fish_clipboard_paste
+  bind -M normal P fish_clipboard_paste
 end
