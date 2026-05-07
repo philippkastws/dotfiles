c10c3a116e6681c11b6246876b8ce851e0304666
philippkastws <philipp@kast.ws>
2026-05-07 02:47:01 +0200

    Wezterm fix warnings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -38,12 +38,12 @@ config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 -- config.window_close_confirmation = "NeverPrompt"
 
 config.window_padding = {
-	left = "12px",
-	right = "12px",
-	top = "1.5cell",
-	bottom = "0.5cell",
+  left = "12px",
+  right = "12px",
+  top = "1.5cell",
+  bottom = "0.5cell",
 }
--- 
+--
 -- config.keys = {
 -- 	{
 -- 		key = "Enter",
@@ -149,22 +149,22 @@ return config
 
 
 -- local wezterm = require("wezterm")
--- 
+--
 -- local config = {}
--- 
+--
 -- if wezterm.config_builder then
 -- 	config = wezterm.config_builder()
 -- end
--- 
+--
 -- -- config.color_scheme = "tokyonight_moon"
 -- config.color_scheme = "nord"
--- 
+--
 -- config.colors = {
 -- 	background = "#232730",
 -- 	cursor_bg = "#4C566A",
 -- 	cursor_border = "#4C566A",
 -- }
--- 
+--
 -- config.font = wezterm.font_with_fallback({
 -- 	{ family = "Berkeley Mono", italic = false },
 -- 	{ family = "Symbols Nerd Font Mono", scale = 0.75 },
@@ -187,21 +187,21 @@ return config
 -- 		}),
 -- 	},
 -- }
--- 
+--
 -- config.use_fancy_tab_bar = false
 -- config.hide_tab_bar_if_only_one_tab = true
 -- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 -- config.macos_window_background_blur = 40
 -- config.window_background_opacity = 0.90
 -- config.window_close_confirmation = "NeverPrompt"
--- 
+--
 -- config.window_padding = {
 -- 	left = "24px",
 -- 	right = "24px",
 -- 	top = "1.5cell",
 -- 	bottom = "0.5cell",
 -- }
--- 
+--
 -- config.keys = {
 -- 	{
 -- 		key = "Enter",
@@ -209,6 +209,6 @@ return config
 -- 		action = wezterm.action.DisableDefaultAssignment,
 -- 	},
 -- }
--- 
+--
 -- return config
--- 
+--
